3fb0abf4dee963f6ea7ca45a85d055ca61775705
Sergio Armando Cazares Ruiz <Cazares250@outlook.com>
2022-03-12 14:08:33 -0700

    Declaracion de un arreglo

--- Demo/Program.cs
+++ Demo/Program.cs
@@ -1,2 +1,3 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] nombrePersonas = { "Juan", "Pedro" };
+Console.WriteLine(nombrePersonas[1]);
+Console.WriteLine();
